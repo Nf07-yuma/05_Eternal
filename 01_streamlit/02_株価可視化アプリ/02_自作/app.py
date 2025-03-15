ae267ba97c8304cfdecf4067943a461fe3de1b57
Nf07-yuma <yuma.build01@gmail.com>
2025-03-15 12:32:31 +0900

    株価可視化アプリ

---
+++ 01_streamlit/02_株価可視化アプリ/02_自作/app.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import yfinance as yf
+import altair as alt
+import streamlit as st
+
+st.title('米国株価可視化アプリ')
+
+st.sidebar.write("""
+# GAFA株価
+こちらは株価可視化ツールです。以下のオプションから表示日数を指定できます。
+""")
+
+st.sidebar.write("""
+## 表示日数選択
+""")
+
+days = st.sidebar.slider('日数', 1, 50, 20)
+
+st.write(f"""
+### 過去 **{days}日間** のGAFA株価
+""")
+
+@st.cache_data
+def get_data(days, tickers):
+    df = pd.DataFrame()
+    for company in tickers.keys():
+        tkr = yf.Ticker(tickers[company])
+        hist = tkr.history(period=f'{days}d')
+        hist.index = hist.index.strftime('%d %B %Y')
+        hist = hist[['Close']]
+        hist.columns = [company]
+        hist = hist.T
+        hist.index.name = 'Name'
+        df = pd.concat([df, hist])
+    return df
+
+@st.cache_resource
+def load_model():
+    # モデルやリソースの読み込み処理
+    model = "リソースやモデルがここに読み込まれます"
+    return model
+
+model = load_model()  # キャッシュされたモデルやリソースを使用
+
+try: 
+    st.sidebar.write("""
+    ## 株価の範囲指定
+    """)
+    ymin, ymax = st.sidebar.slider(
+        '範囲を指定してください。',
+        0.0, 3500.0, (0.0, 1500.0) # 3500.0 -> 500.0
+    )
+
+    tickers = {
+        'apple': 'AAPL',
+        'google': 'GOOGL',
+        'microsoft': 'MSFT',
+        'netflix': 'NFLX',
+        'amazon': 'AMZN'
+    }
+    df = get_data(days, tickers)
+    companies = st.multiselect(
+        '会社名を選択してください。',
+        list(df.index),
+        ['google', 'amazon', 'apple']
+    )
+
+    if not companies:
+        st.error('少なくとも一社は選んでください。')
+    else:
+        data = df.loc[companies]
+        st.write("### 株価 (USD)", data.sort_index())
+        data = data.T.reset_index()
+        data = pd.melt(data, id_vars=['Date']).rename(
+            columns={'value': 'Stock Prices(USD)'}
+        )
+        chart = (
+            alt.Chart(data)
+            .mark_line(opacity=0.8, clip=True)
+            .encode(
+                x="Date:T",
+                y=alt.Y("Stock Prices(USD):Q", stack=None, scale=alt.Scale(domain=[ymin, ymax])),
+                color='Name:N'
+            )
+        )
+        st.altair_chart(chart, use_container_width=True)
+except:
+    st.error(
+        "おっと！なにかエラーが起きているようです。"
+    )
+
+
+# st.cache is deprecated and will be removed soon. Please use one of Streamlit's new caching commands, st.cache_data or st.cache_resource. More information in our docs.
+# Note: The behavior of st.cache was updated in Streamlit 1.36 to the new caching logic used by st.cache_data and st.cache_resource. This might lead to some problems or unexpected behavior in certain edge cases.
